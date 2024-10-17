4203bd56a00900eded4816a83cbda22f1fdf8f37
JoanSebastianRuiz <joansebastianruizangarita10@gmail.com>
2024-10-17 17:22:12 -0500

    feat: :sparkles: Commit

--- back/conexion.js
+++ back/conexion.js
@@ -4,7 +4,7 @@ npm install pg
 
 */
 
-const {Pool} = require("pg");
+/* const {Pool} = require("pg");
 
 const pool = new Pool({
     user: "postgres",
@@ -27,4 +27,44 @@ const ejecutarQuery = async(query)=>{
     }
 }
 
-module.exports.ejecutarQuery=ejecutarQuery;
+module.exports.ejecutarQuery=ejecutarQuery; */
+
+
+const mysql = require('mysql2/promise');
+
+async function connectToDatabase() {
+  try {
+    const connection = await mysql.createConnection({
+      host: 'localhost',      // Cambia a tu host de MySQL
+      user: 'campus2023',           // Cambia a tu usuario de MySQL
+      password: 'campus2023', // Cambia a tu contraseña
+      database: 'billup' // Cambia al nombre de tu base de datos
+    });
+
+    console.log('Conexión exitosa a la base de datos');
+    return connection;
+  } catch (err) {
+    console.error('Error conectando a la base de datos:', err.message);
+    throw err;
+  }
+}
+
+async function ejecutarQuery(query) {
+    try {
+      const connection = await connectToDatabase();
+      
+      // Hacer una consulta a la base de datos
+      const [rows, fields] = await connection.execute(query);
+
+      console.log('Resultados de la consulta:', rows);
+
+      // Cerrar la conexión después de usarla
+      await connection.end();
+      return rows;
+
+    } catch (err) {
+      console.error('Error ejecutando la consulta:', err.message);
+    }
+  }
+
+  module.exports.ejecutarQuery=ejecutarQuery;
